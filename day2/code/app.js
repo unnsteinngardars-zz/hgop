b4f9b81ef080b399188bf9e348ba471fb56770ad
Unnsteinn Garðarsson <unnsteinng@gmail.com>
2017-11-28 17:06:17 +0000

    We worked on this as a pair, Disa had some issues with linux and we had to spend sometime along with this project on preparing installation of a virtual machine, we can assure you that this was all done by teamworking

---
+++ day2/code/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const redis = require('redis');
+
+var app = express();
+var client = redis.createClient(6379, 'my_redis_container', {
+  retry_strategy: options => {
+    return;
+  },
+});
+
+app.get('/', (req, res) => {
+  if (client.connected) {
+    client.incr('page_load_count', (error, reply) => {
+      var msg = 'Connected to redis, you are awesome :D' + 'Page loaded ' + reply + ' times!';
+      res.statusCode = 200;
+      res.send(msg);
+      return;
+    });
+  } else {
+    var msg = "Failed to connect to redis :'(";
+    res.statusCode = 500;
+    res.send(msg);
+  }
+});
+
+app.listen(3000);
