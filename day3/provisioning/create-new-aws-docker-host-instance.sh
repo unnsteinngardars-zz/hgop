b7a23cdb40e820cbbd9209c44b24e46dcb9d4ab2
disaerna <unnsteinng@gmail.com>
2017-12-01 12:03:33 +0000

    scripts for day3 work, need to be commented

--- day3/provisioning/create-new-aws-docker-host-instance.sh
+++ day3/provisioning/create-new-aws-docker-host-instance.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -xv
 SECURITY_GROUP_NAME="IheartAWS"
-echo ${SECURITY_GROUP_NAME} > ./ec2_instance/security-group-name.txt
 IMAGE_ID="ami-15e9c770"
 INSTANCE_DIR="ec2_instance"
 if [ -d "${INSTANCE_DIR}" ]; then
@@ -8,13 +7,16 @@ if [ -d "${INSTANCE_DIR}" ]; then
 fi
 
 [ -d "${INSTANCE_DIR}" ] || mkdir ${INSTANCE_DIR}
+#creates security-grou-name.txt file in ec2_instance folder
+echo ${SECURITY_GROUP_NAME} > ./ec2_instance/security-group-name.txt
 
-#create aws ec2 key-pair , and output to folder ec2_instance. also change modification on file to fix permission issues
+
+#create aws ec2 key-pair and changes modification on file to fix permission issues
+#creates security-group-name.pem file for later use with accessing aws machine
 echo "execute create-key-pair for security group $SECURITY_GROUP_NAME"
 aws ec2 create-key-pair --key-name ${SECURITY_GROUP_NAME} --query 'KeyMaterial' --output text > ${INSTANCE_DIR}/${SECURITY_GROUP_NAME}.pem
 chmod 400 ${INSTANCE_DIR}/${SECURITY_GROUP_NAME}.pem
 echo "key-pair created successfully"
-
 
 #get security group id from security group from created in the command and outputs the key to the screen
 echo "create security group $SECURITY_GROUP_NAME"
@@ -32,6 +34,8 @@ MY_CIDR=${MY_PUBLIC_IP}/32
 #authorize security group
 echo "authorizing $SECURITY_GROUP_NAME"
 aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 80 --cidr ${MY_CIDR}
+aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${MY_CIDR}
+
 
 
 #runs an instance from a seperated instance init script and fetches the generated instance key
@@ -40,7 +44,7 @@ INSTANCE_ID=$(aws ec2 run-instances --user-data file://ec2-instance-init.sh --im
 echo ${INSTANCE_ID} > ./ec2_instance/instance-id.txt
 echo "instance ID fetched successfully"
 
-#something
+#lunch instance on aws ec2
 echo "Lunch instance"
 aws ec2 wait --region us-east-2 instance-running --instance-ids ${INSTANCE_ID}
 export INSTANCE_PUBLIC_NAME=$(aws ec2 describe-instances --instance-ids ${INSTANCE_ID} --query "Reservations[*].Instances[*].PublicDnsName" --output=text)
